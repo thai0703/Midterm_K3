21a9850e107330d9ffe022ef96248f7281233009
thai0703 <thai070306@gmail.com>
2024-08-27 12:00:02 -0500

    final

---
+++ data.js
@@ -0,0 +1,5 @@
+const listApiKey = [];
+
+export{
+    listApiKey
+}
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import express from 'express'
+import express, { text } from 'express'
 import mongoose from 'mongoose'
 import usersModel from './model/user.js';
 import postsModel from './model/post.js';
 import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
-import { error } from 'console';
+import crypto from 'crypto'
+import { listApiKey } from './data.js';
+
 dotenv.config();
 await mongoose.connect(process.env.MONGODB_URL)
 
@@ -31,7 +33,6 @@ app.post('/users/register', async (req, res)=>{
             email: email,
             password: hashedPassword
         })
-        usersModel.push(createdUser)
         res.status(201).send({
             message: 'Register successful!',
             data: createdUser
@@ -57,9 +58,16 @@ app.post('/users/login', async (req, res)=>{
         if(!crrUser) throw new Error('email or password is invalid')
         const comparedPassword = bcrypt.compareSync(password, crrUser.password)
         if(!comparedPassword) throw new Error('Email or password is invalid')
+        const findExistKey = listApiKey.findIndex((item) => String(item).includes(email) && String(item).includes(crrUser._id))
+        if(findExistKey >= 0){
+            listApiKey.splice(findExistKey, 1)
+        }
+        const randomString = crypto.randomUUID()
+        const generateApiKey = `mern-$${crrUser._id}$-$${email}$-$${randomString}$`
+        listApiKey.push(generateApiKey)
         res.status(201).send({
             message: 'Login successful!',
-            data: crrUser
+            data: listApiKey
         })
     } catch(error){
         res.status(400).send({
@@ -70,23 +78,60 @@ app.post('/users/login', async (req, res)=>{
 })
 
 //Cau 3: Tao post
-// Ví dụ: /posts?apiKey=mern-$507f1f77bcf86cd799439011$-$nguyena@gmail.com$-$9bb4d-3b7d-4ad-9bdd-2d7dcb6d$
-//Lấy apiKey t
+
 
 app.post('/posts', async (req, res)=>{
     try{
-        const {userID, content} = req.body; 
-        if(!userID || !content){
-            throw new Error('userId or content is missing!')
+        const {content} = req.body; 
+        const apiKey = String(Object.values(req.query))
+        let text = apiKey.split("-")[1]
+        let result = text.slice(1, text.length - 1)
+        if(!apiKey) throw new Error('Cannot validate user')
+        const findApiKey = listApiKey.findIndex((item) => item == apiKey)
+        if(findApiKey < 0) throw new Error('Cannot validate user')
+        if(!content){
+            throw new Error('Content is missing!')
         }
         const createdPost = await postsModel.create({
-            userID: userID,
+            userID: result,
             content: content
         })
-        postsModel.push(createdPost)
         res.status(201).send({
             message: 'Post created successfully!',
-            data: createdUser
+            data: createdPost
+        })
+    } catch(error){
+        res.status(400).send({
+            message: error.message,
+            data: null
+        })
+    }
+})
+
+//Cau 4: Update post
+app.put('/posts:id', async (req, res)=>{
+    try{
+        const {content} = req.body; 
+        const postID = String(Object.values(req.params))
+        const apiKey = String(Object.values(req.query))
+        // let text = apiKey.split("-")[1]
+        // let result = text.slice(1, text.length - 1)
+        if(!apiKey) throw new Error('Cannot validate user')
+        const findApiKey = listApiKey.findIndex((item) => item == apiKey)
+        if(findApiKey < 0) throw new Error('Cannot validate user')
+        const crrPost = await postsModel.findOne({
+            _id: postID
+        })
+        if(!crrPost) throw new Error('Cannot find post')
+        if(!content){
+            throw new Error('Content is missing!')
+        }
+        crrPost.content = content;
+        crrPost.updatedAt = Date.now();
+        await crrPost.save();
+        res.status(201).send({
+            message: 'Post updated successfully!',
+            data: crrPost
         })
     } catch(error){
         res.status(400).send({
